3ab44997ec36c1e5ee3b10a74254e421cff9d8c3
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-21 21:26:02 +0300

    Create empty Spring Boot application with Spring Security and JWT

---
+++ src/main/java/ru/iooko/LiquibaseJWT/LiquibaseJwtApplication.java
@@ -0,0 +1,13 @@
+package ru.iooko.LiquibaseJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/iooko/LiquibaseJWT/LiquibaseJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iooko.LiquibaseJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
