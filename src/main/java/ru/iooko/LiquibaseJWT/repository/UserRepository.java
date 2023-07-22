f8f118b6c6393417b2594c52980c64ad2dd91e10
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-22 14:10:15 +0300

    Add UserServiceImpl

--- src/main/java/ru/iooko/LiquibaseJWT/repository/UserRepository.java
+++ src/main/java/ru/iooko/LiquibaseJWT/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package ru.iooko.LiquibaseJWT.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.iooko.LiquibaseJWT.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String name);
 }
---
+++ src/main/java/ru/iooko/LiquibaseJWT/service/impl/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package ru.iooko.LiquibaseJWT.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.iooko.LiquibaseJWT.model.Role;
+import ru.iooko.LiquibaseJWT.model.User;
+import ru.iooko.LiquibaseJWT.repository.RoleRepository;
+import ru.iooko.LiquibaseJWT.repository.UserRepository;
+import ru.iooko.LiquibaseJWT.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.iooko.LiquibaseJWT.model.Status.ACTIVE;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User register(User user) {
+        // By default, the user is assigned the role "USER"
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles = new ArrayList<>();
+        userRoles.add(roleUser);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(ACTIVE);
+
+        User registeredUser = userRepository.save(user);
+        log.info("IN register - user: {} successfully registered", registeredUser);
+        return registeredUser;
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> result = userRepository.findAll();
+        log.info("IN getAll - {} users found", result.size());
+        return result;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUsername - user: {} found by username: {}", result, username);
+        return result;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User result = userRepository.findById(id).orElse(null);
+
+        if (result == null) {
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        log.info("IN findById - user: {} found by id: {}", result, id);
+        return result;
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id: {} successfully deleted", id);
+    }
+}
