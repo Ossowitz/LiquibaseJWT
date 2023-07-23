75bc0be8824f4dd7cb710ba3a2f3ceb6bbc2a9a2
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-23 15:24:31 +0300

    Add JwtUserFactory

---
+++ src/main/java/ru/iooko/LiquibaseJWT/security/jwt/JwtUserFactory.java
@@ -0,0 +1,40 @@
+package ru.iooko.LiquibaseJWT.security.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import ru.iooko.LiquibaseJWT.model.Role;
+import ru.iooko.LiquibaseJWT.model.Status;
+import ru.iooko.LiquibaseJWT.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static ru.iooko.LiquibaseJWT.model.Status.*;
+
+public final class JwtUserFactory {
+
+    public JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(new ArrayList<>(user.getRoles())),
+                user.getStatus().equals(ACTIVE),
+                user.getUpdated()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> userRoles) {
+        return userRoles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(toList());
+    }
+}
